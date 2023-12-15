54b9ed3bd74677a0638ac5435b40e66ac8747a8b
Wilbert José Girón Pérez <USSS039723@ugb.edu.sv>
2023-12-15 16:32:44 -0600

    Tala

--- ExamendeRp/Form1.cs
+++ ExamendeRp/Form1.cs
@@ -16,5 +16,12 @@ namespace ExamendeRp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'datasetAdm.tbl_vehiculos' Puede moverla o quitarla según sea necesario.
+            this.tbl_vehiculosTableAdapter.Fill(this.datasetAdm.tbl_vehiculos);
+
+        }
     }
 }
